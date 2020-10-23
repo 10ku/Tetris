8b575da06d9017fbed4beed18969b43806c5b798
10ku <keprosj@gmail.com>
2020-10-22 22:18:14 -0500

    Added some imports

--- tetris/Tetris.java
+++ tetris/Tetris.java
@@ -1,5 +1,14 @@
 package tetris;
 
+import java.util.Arrays;
+import java.util.Timer;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+import javax.swing.*;
+
 public class Tetris
 {
 	public static void main(String[] args)
