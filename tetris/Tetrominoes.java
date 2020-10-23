a638421cec942effe807ae8fcaebfba35e8241e3
10ku <keprosj@gmail.com>
2020-10-22 22:28:57 -0500

    Added Tetrominoes class and the tetromino shapes

---
+++ tetris/Tetrominoes.java
@@ -0,0 +1,43 @@
+package tetris;
+
+public class Tetrominoes
+{
+	public Tetrominoes()
+	{
+		int[][] iBlock = {
+			{0,0,0,0}, //x
+			{0,1,2,3}, //y
+					};
+					
+		int[][] jBlock ={
+			{0,0,1,2}, 
+			{0,1,0,0}, 
+					};
+		
+		int[][] lBlock = {
+			{0,1,2,2}, 
+			{0,0,0,1}, 
+					};
+					
+		int[][] oBlock = {
+			{0,0,1,1}, 
+			{0,1,0,1}, 
+					};
+					
+		int[][] sBlock = {
+			{0,1,1,2}, 
+			{0,0,1,1}, 
+					};
+		
+		int[][] tBlock = {
+			{0,1,1,2}, 
+			{0,0,1,0}, 
+					};
+					
+		int[][] zBlock = {
+			{0,1,1,2}, 
+			{1,1,0,0}, 
+					};
+	
+	}
+}
