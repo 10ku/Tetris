006b0c22d1d2015ba3e20add48bc60c01a84351c
10ku <keprosj@gmail.com>
2020-10-22 22:23:04 -0500

    added toString()

--- tetris/Board.java
+++ tetris/Board.java
@@ -27,4 +27,22 @@ public class Board
 				{'|','.','.','.','.','.','.','.','.','.','.','|'}
 																};
 	}
+	
+	public String toString()
+	{
+		String boardString = "";
+		
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[0].length; j++)
+			{
+				boardString += board[i][j];
+				if (j + 1 == board[0].length)
+				{
+					boardString += "\n";
+				}
+			}
+		}
+		return boardString;
+	}
 }
