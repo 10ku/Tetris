13deb565a67f952ce61b9aaff55f70d23af2dfd6
10ku <keprosj@gmail.com>
2020-10-28 21:39:19 -0500

    Create GUI to replace console

--- tetris/Tetris.java
+++ tetris/Tetris.java
@@ -2,25 +2,51 @@ package tetris;
 
 import java.util.Arrays;
 import java.util.Timer;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.*;
-
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.PrintStream;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Tetris
 {
 	public static void main(String[] args)
 	{
+		//GUI declarations
+		JFrame frame = new JFrame("Tetris");
+		JTextArea textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		PrintStream printStream = new PrintStream(new Output(textArea));
+		
+		//GUI parameters
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		textArea.setBackground(Color.BLACK);
+		textArea.setForeground(Color.WHITE);
+		textArea.setFont(new Font("Courier New", Font.PLAIN, 16));
+		textArea.setEditable(false);
+		textArea.setFocusable(false);
+		frame.add(scrollPane);
+		frame.setSize(640, 480);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		//GUI MISC
+		System.setOut(printStream);
+		System.setErr(printStream);
+		
 		Board myBoard = new Board();
+		boolean quit = false;
+		
 		System.out.println(myBoard.toString());
 		
 		Tetrominoes test = new Tetrominoes();
 		int[][] testM = {
-				{0,1,1,2}, 
-				{1,1,0,0}, 
+				{0,1,1,2},
+				{1,1,0,0},
 						};
-		System.out.println(Arrays.deepToString(test.rotate90(testM)));
+		
+		//jFrame.dispose();
 	}
 }
