2a9667e935e1031aa96873634d53958c39aae10f
10ku <keprosj@gmail.com>
2020-10-28 21:24:26 -0500

    Add Output class
    This will be used to redirect Java's standard output to a JtextArea.

---
+++ tetris/Output.java
@@ -0,0 +1,22 @@
+package tetris;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.swing.JTextArea;
+
+public class Output extends OutputStream
+{
+	private JTextArea textArea;
+	
+	public Output(JTextArea textArea)
+	{
+		this.textArea = textArea;
+	}
+
+	@Override
+	public void write(int b) throws IOException
+	{
+		this.textArea.append(String.valueOf((char)b));
+		this.textArea.setCaretPosition(textArea.getDocument().getLength());
+	}
+}
