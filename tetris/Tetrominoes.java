56987fddb2052fa7feef75f363c3f8cec0e7d6b7
10ku <keprosj@gmail.com>
2020-11-06 00:20:58 -0600

    Add function for returning random blocks

--- tetris/Tetrominoes.java
+++ tetris/Tetrominoes.java
@@ -1,46 +1,72 @@
 package tetris;
 
+import java.util.Random;
+
 public class Tetrominoes
 {
 	private final static int[][] iBlock = {
-			{0,0,0,0}, //x
-			{0,1,2,3}, //y
-					};
-					
+		{0,0,0,0}, //x
+		{0,1,2,3}, //y
+				};
+				
 	private final static int[][] jBlock = {
-			{0,0,1,2}, 
-			{0,1,0,0}, 
-					};
-		
-	private final static int[][] lBlock = {
-			{0,1,2,2}, 
-			{0,0,0,1}, 
-					};
-					
-	private final static int[][] oBlock = {
-			{0,0,1,1}, 
-			{0,1,0,1}, 
-					};
-					
-	private final static int[][] sBlock = {
-			{0,1,1,2}, 
-			{0,0,1,1}, 
-					};
-		
-	private final static int[][] tBlock = {
-			{0,1,1,2}, 
-			{0,0,1,0}, 
-					};
-					
-	private final static int[][] zBlock = {
-			{0,1,1,2}, 
-			{1,1,0,0}, 
-					};
+		{0,0,1,2}, 
+		{0,1,0,0}, 
+				};
 	
+	private final static int[][] lBlock = {
+		{0,1,2,2}, 
+		{0,0,0,1}, 
+				};
+				
+	private final static int[][] oBlock = {
+		{0,0,1,1}, 
+		{0,1,0,1}, 
+				};
+				
+	private final static int[][] sBlock = {
+		{0,1,1,2}, 
+		{0,0,1,1}, 
+				};
+	
+	private final static int[][] tBlock = {
+		{0,1,1,2}, 
+		{0,0,1,0}, 
+				};
+				
+	private final static int[][] zBlock = {
+		{0,1,1,2}, 
+		{1,1,0,0}, 
+				};
+	
+	private Random random = new Random();
 	
 	public Tetrominoes()
 	{
 		
+	}
+	
+	public int[][] returnRandomTetromino()
+	{
+		switch (random.nextInt(7))
+		{
+		case 0:
+			return iBlock.clone();
+		case 1:
+			return jBlock.clone();
+		case 2:
+			return lBlock.clone();
+		case 3:
+			return oBlock.clone();
+		case 4:
+			return sBlock.clone();
+		case 5:
+			return tBlock.clone();
+		case 6:
+			return zBlock.clone();
+		default:
+			return iBlock.clone();
+		}
 	}
 	
 	public int[][] rotate90(int[][] secondMatrix)
@@ -60,6 +86,7 @@ public class Tetrominoes
 				rotatedMatrix[currRow][currColumn] = firstMatrix[currRow][0] * secondMatrix[0][currColumn] + firstMatrix[currRow][1] * secondMatrix[1][currColumn];
 			}
 		}
+
 		return rotatedMatrix;
 	}
 }
