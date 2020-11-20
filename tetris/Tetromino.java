4942923d05725902b3bb936712b636eea3dbaf32
10ku <keprosj@gmail.com>
2020-11-20 02:11:39 -0600

    Move rotate90 function into Tetromino class

--- tetris/Tetromino.java
+++ tetris/Tetromino.java
@@ -10,4 +10,25 @@ public class Tetromino
 	{
 		this.tetromino = tetromino;
 	}
+	
+	public void rotate90()
+	{
+		int row = 2;
+		int column = tetromino[0].length;
+		int[][] rotatedMatrix = new int[row][column];
+		int[][] rotationMatrix = {
+			{ 0,1}, 
+			{-1,0}, 
+				 };
+		
+		for (int currRow = 0; currRow < row; currRow++)
+		{
+			for (int currColumn = 0; currColumn < column; currColumn++)
+			{
+				rotatedMatrix[currRow][currColumn] = rotationMatrix[currRow][0] * tetromino[0][currColumn] + rotationMatrix[currRow][1] * tetromino[1][currColumn];
+			}
+		}
+
+		tetromino = rotatedMatrix;
+	}
 }
